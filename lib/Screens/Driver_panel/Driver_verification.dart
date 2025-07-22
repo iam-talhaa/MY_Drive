760227a825f228e85880a0e23d43c3ad95d7d18c
unknown <tahakhan4141@gmail.com>
2025-07-22 22:55:29 +0500

    Added Driver Verification Screen

---
+++ lib/Screens/Driver_panel/Driver_verification.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DriverVerification extends StatefulWidget {
+  const DriverVerification({super.key});
+
+  @override
+  State<DriverVerification> createState() => _DriverVerificationState();
+}
+
+class _DriverVerificationState extends State<DriverVerification> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Column(children: []));
+  }
+}
